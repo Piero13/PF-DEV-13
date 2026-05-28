1d6c4b311ab59339a9532ef64bf01558b651df53
Pierre Fasce <lemonpiero@gmail.com>
2026-05-28 21:37:29 +0200

    modify open graph

--- src/components/shared/SEO/Seo.jsx
+++ src/components/shared/SEO/Seo.jsx
@@ -24,32 +24,6 @@ function Seo({
         content={description}
       />
 
-      {/* Open Graph */}
-      <meta
-        property="og:title"
-        content={fullTitle}
-      />
-
-      <meta
-        property="og:description"
-        content={description}
-      />
-
-      <meta
-        property="og:type"
-        content="website"
-      />
-
-      <meta
-        property="og:url"
-        content={url}
-      />
-
-      <meta
-        property="og:image"
-        content={image}
-      />
-
       {/* Twitter */}
       <meta
         name="twitter:card"
